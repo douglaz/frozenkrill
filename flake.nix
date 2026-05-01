733afc6e968b8db6e078d148385895dae3a0c550
douglaz <allandouglas@gmail.com>
2026-04-30 20:56:51 -0700

    chore(nix): use nixpkgs rust, keep musl cross on x86_64
    rust-overlay fetches Rust as .tar.xz and unpacks it inside a Nix
    builder. On filesystems where tar's mkdir+chmod sequence fails
    (notably Android via PRoot), `nix develop` breaks with "Cannot
    change mode: No such file or directory".
    
    Switch to nixpkgs' rustc/cargo/clippy/rustfmt/rust-analyzer -
    substituted as pre-built outputs, no in-builder unpacking.
    
    Behavior preserved: on x86_64-linux, the musl cross-compile setup
    is unchanged. On other systems, CARGO_BUILD_TARGET is set to the
    host triple to override the repo's .cargo/config.toml musl pin.

--- flake.nix
+++ flake.nix
@@ -1,38 +1,40 @@
 {
-  description = "Rust project with musl target";
+  description = "Rust project with musl target on x86_64, host toolchain elsewhere";
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
-    rust-overlay.url = "github:oxalica/rust-overlay";
   };
 
-  outputs = { self, nixpkgs, flake-utils, rust-overlay }:
+  outputs = { self, nixpkgs, flake-utils }:
     flake-utils.lib.eachDefaultSystem (system:
       let
-        pkgs = import nixpkgs {
-          inherit system;
-          overlays = [ (import rust-overlay) ];
-        };
-        
-        rustToolchain = pkgs.rust-bin.stable.latest.default.override {
-          extensions = [ "rust-src" "rust-analyzer" ];
-          targets = [ "x86_64-unknown-linux-musl" ];
-        };
+        pkgs = import nixpkgs { inherit system; };
+        isX86_64Linux = system == "x86_64-linux";
+        muslCC = pkgs.pkgsStatic.stdenv.cc;
       in
       {
-        devShells.default = pkgs.mkShell {
+        devShells.default = pkgs.mkShell ({
           packages = with pkgs; [
             bashInteractive
-            rustToolchain
+            rustc
+            cargo
+            rustfmt
+            clippy
+            rust-analyzer
             pkg-config
-            pkgsStatic.stdenv.cc
             cargo-edit
-          ];
+          ] ++ pkgs.lib.optionals isX86_64Linux [ muslCC ];
 
-          CARGO_TARGET_X86_64_UNKNOWN_LINUX_MUSL_LINKER = "${pkgs.pkgsStatic.stdenv.cc}/bin/${pkgs.pkgsStatic.stdenv.cc.targetPrefix}cc";
-          CC_x86_64_unknown_linux_musl = "${pkgs.pkgsStatic.stdenv.cc}/bin/${pkgs.pkgsStatic.stdenv.cc.targetPrefix}cc";
-        };
+          RUST_SRC_PATH = "${pkgs.rustPlatform.rustLibSrc}";
+
+          shellHook = pkgs.lib.optionalString (!isX86_64Linux) ''
+            export CARGO_BUILD_TARGET="$(rustc -vV | sed -n 's/^host: //p')"
+          '';
+        } // pkgs.lib.optionalAttrs isX86_64Linux {
+          CARGO_TARGET_X86_64_UNKNOWN_LINUX_MUSL_LINKER = "${muslCC}/bin/${muslCC.targetPrefix}cc";
+          CC_x86_64_unknown_linux_musl = "${muslCC}/bin/${muslCC.targetPrefix}cc";
+        });
       }
     );
 }
